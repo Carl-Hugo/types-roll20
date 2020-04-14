50c0fb2354c97739b66bb1d8caef8645a3da6518
Layton <laytonb@hotmail.co.uk>
2020-04-14 15:57:58 +0100

    Corrected on change return types

--- index.d.ts
+++ index.d.ts
@@ -566,9 +566,9 @@ declare function log(message: any): void;
  */
 declare function on(event: "ready", callback: () => void): void;
 declare function on(event: "chat:message", callback: (msg: ChatEventData) => void): void;
-declare function on(event: "change:graphic", callback: (obj: Graphic, prev: Graphic) => void): void;
-declare function on(event: "change:character", callback: (obj: Character, prev: Character) => void): void;
-declare function on(event: "change:attribute", callback: (obj: Attribute, prev: Attribute) => void): void;
+declare function on(event: "change:graphic", callback: (obj: Roll20ObjectBaseProperties, prev: Roll20ObjectBaseProperties) => void): void;
+declare function on(event: "change:character", callback: (obj: Roll20ObjectBaseProperties, prev: Roll20ObjectBaseProperties) => void): void;
+declare function on(event: "change:attribute", callback: (obj: Roll20ObjectBaseProperties, prev: Roll20ObjectBaseProperties) => void): void;
 declare function on(event: "change:campaign:playerpageid", callback: () => void): void;
 declare function on(event: "change:campaign:turnorder", callback: (obj: Campaign, prev: CampaignImmutableSynchronousGetProperties & CampaignMutableSynchronousGetProperties) => void): void;
 
